83c10c9ec08dd5bab8fdae0c5ed9e5d163675f3b
Nicholas Carsner <nicholascarsner@gmail.com>
2024-01-24 20:36:02 -0600

    Minor updates.

---
+++ leagues.py
@@ -0,0 +1,25 @@
+game_rates = {
+    "ovl": 150,
+    "ccua_hs": 90,
+    "ccua_ms": 80,
+    "ccua_post": 85,
+    "mtaba": 90,
+    "mtaba_post": 95,
+    "tru": 90,
+    "tru_fall": 85,
+    "ll_majors": 55,
+    "ll_minors": 45,
+}
+
+game_assignors = {
+    "ovl": "TruBlu",
+    "tssaa_hs": "CCUA",
+    "tssaa_ms": "CCUA",
+    "tssaa_post": "CCUA",
+    "mtaba": "4 Star Sports",
+    "mtaba_post": "4 Star Sports",
+    "trublu": "TruBlu",
+    "trublu_fall": "TruBlu",
+    "ll_majors": "4 Star Sports",
+    "ll_minors": "4 Star Sports",
+}
